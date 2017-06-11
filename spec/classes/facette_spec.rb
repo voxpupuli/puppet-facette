0eaae09aefa5a047ac16a19651f853d97b249243
Tim Meusel <tim@bastelfreak.de>
2017-06-11 14:51:49 +0200

    migrate tests to rspec-puppet-facts
    the puppetlabs-apt dependency uses several facts which we need to mock.
    Using rspec-puppet-facts is the easiest way to get the facts.

--- spec/classes/facette_spec.rb
+++ spec/classes/facette_spec.rb
@@ -5,217 +5,209 @@ describe 'facette' do
     'include apt'
   end
 
-  describe 'on Ubuntu' do
-    let(:facts) do
-      {
-        lsbdistcodename:     'trusty',
-        lsbdistrelease:      '14.04',
-        lsbmajdistrelease:   '14.04',
-        lsbdistdescription:  'Ubuntu 14.04.3 LTS',
-        lsbdistid:           'Ubuntu',
-        osfamily:            'Debian',
-        operatingsystem:     'Ubuntu'
-      }
-    end
-
-    context 'defaults' do
-      it { is_expected.to contain_apt__ppa('ppa:facette/ppa') }
-      it { is_expected.to contain_class('facette') }
-      it { is_expected.to contain_class('facette::params') }
-      it { is_expected.to compile.with_all_deps }
-
-      it do
-        is_expected.to contain_package('facette').with(
-          ensure: 'installed',
-          notify: 'Service[facette]',
-          require: 'Apt::Ppa[ppa:facette/ppa]'
-        )
+  on_supported_os.each do |os, facts|
+    context "on #{os} " do
+      let :facts do
+        facts
       end
 
-      it do
-        is_expected.to contain_shellvar('enable facette').with(
-          ensure: 'present',
-          target: '/etc/default/facette',
-          variable: 'ENABLED',
-          value: 'true',
-          require: 'Package[facette]',
-          notify: 'Service[facette]'
-        )
+      context 'defaults' do
+        it { is_expected.to contain_apt__ppa('ppa:facette/ppa') }
+        it { is_expected.to contain_class('facette') }
+        it { is_expected.to contain_class('facette::params') }
+        it { is_expected.to compile.with_all_deps }
+
+        it do
+          is_expected.to contain_package('facette').with(
+            ensure: 'installed',
+            notify: 'Service[facette]',
+            require: 'Apt::Ppa[ppa:facette/ppa]'
+          )
+        end
+
+        it do
+          is_expected.to contain_shellvar('enable facette').with(
+            ensure: 'present',
+            target: '/etc/default/facette',
+            variable: 'ENABLED',
+            value: 'true',
+            require: 'Package[facette]',
+            notify: 'Service[facette]'
+          )
+        end
+
+        it do
+          is_expected.to contain_service('facette').with(
+            ensure: 'running',
+            enable: true
+          )
+        end
+
+        it do
+          is_expected.to contain_file('facette.json').with(
+            ensure: 'file',
+            path: '/etc/facette/facette.json',
+            require: 'Package[facette]',
+            notify: 'Service[facette]',
+            owner: 'root',
+            group: 'root',
+            mode: '0644'
+          ).with_content(
+            %r{"bind": ".+"}
+          ).with_content(
+            %r{"base_dir": ".+"}
+          ).with_content(
+            %r{"providers_dir": ".+"}
+          ).with_content(
+            %r{"data_dir": ".+"}
+          ).with_content(
+            %r{"pid_file": ".+"}
+          )
+        end
       end
 
-      it do
-        is_expected.to contain_service('facette').with(
-          ensure: 'running',
-          enable: true
-        )
+      context 'with package state set to purged' do
+        let(:params) { { state: { 'package' => 'purged' } } }
+
+        it { is_expected.to contain_package('facette').with_ensure('purged') }
       end
 
-      it do
-        is_expected.to contain_file('facette.json').with(
-          ensure: 'file',
-          path: '/etc/facette/facette.json',
-          require: 'Package[facette]',
-          notify: 'Service[facette]',
-          owner: 'root',
-          group: 'root',
-          mode: '0644'
-        ).with_content(
-          %r{"bind": ".+"}
-        ).with_content(
-          %r{"base_dir": ".+"}
-        ).with_content(
-          %r{"providers_dir": ".+"}
-        ).with_content(
-          %r{"data_dir": ".+"}
-        ).with_content(
-          %r{"pid_file": ".+"}
-        )
+      context 'with service state set to stopped' do
+        let(:params) { { state: { 'service' => 'stopped' } } }
+
+        it { is_expected.to contain_service('facette').with_ensure('stopped') }
       end
-    end
 
-    context 'with package state set to purged' do
-      let(:params) { { state: { 'package' => 'purged' } } }
+      context 'with config where base_dir is set to var' do
+        let(:params) { { config: { 'base_dir' => '/var' } } }
 
-      it { is_expected.to contain_package('facette').with_ensure('purged') }
-    end
-
-    context 'with service state set to stopped' do
-      let(:params) { { state: { 'service' => 'stopped' } } }
-
-      it { is_expected.to contain_service('facette').with_ensure('stopped') }
-    end
-
-    context 'with config where base_dir is set to var' do
-      let(:params) { { config: { 'base_dir' => '/var' } } }
-
-      it do
-        is_expected.to contain_file('facette.json').with_content(
-          %r{"bind": ".+"}
-        ).with_content(
-          %r{"base_dir": "/var"}
-        ).with_content(
-          %r{"providers_dir": ".+"}
-        ).with_content(
-          %r{"data_dir": ".+"}
-        ).with_content(
-          %r{"pid_file": ".+"}
-        )
+        it do
+          is_expected.to contain_file('facette.json').with_content(
+            %r{"bind": ".+"}
+          ).with_content(
+            %r{"base_dir": "/var"}
+          ).with_content(
+            %r{"providers_dir": ".+"}
+          ).with_content(
+            %r{"data_dir": ".+"}
+          ).with_content(
+            %r{"pid_file": ".+"}
+          )
+        end
       end
-    end
 
-    context 'with single provider' do
-      let(:params) do
-        {
-          providers:
+      context 'with single provider' do
+        let(:params) do
           {
-            'collectd' =>
+            providers:
             {
-              'connector' =>
+              'collectd' =>
               {
-                'path' => '/var/lib/collectd/rrd',
-                'type' => 'rrd'
+                'connector' =>
+                {
+                  'path' => '/var/lib/collectd/rrd',
+                  'type' => 'rrd'
+                }
               }
             }
           }
-        }
+        end
+
+        it do
+          is_expected.to contain_file('facette-collectd.json').with(
+            ensure: 'file',
+            path: '/etc/facette/providers/collectd.json',
+            require: 'Package[facette]',
+            notify: 'Service[facette]',
+            owner: 'root',
+            group: 'root',
+            mode: '0644'
+          ).with_content(
+            %r{"connector":}
+          ).with_content(
+            %r{"path": "/var/lib/collectd/rrd"}
+          ).with_content(
+            %r{"type": "rrd"}
+          )
+        end
       end
 
-      it do
-        is_expected.to contain_file('facette-collectd.json').with(
-          ensure: 'file',
-          path: '/etc/facette/providers/collectd.json',
-          require: 'Package[facette]',
-          notify: 'Service[facette]',
-          owner: 'root',
-          group: 'root',
-          mode: '0644'
-        ).with_content(
-          %r{"connector":}
-        ).with_content(
-          %r{"path": "/var/lib/collectd/rrd"}
-        ).with_content(
-          %r{"type": "rrd"}
-        )
-      end
-    end
-
-    context 'with multiple providers' do
-      let(:params) do
-        {
-          providers:
+      context 'with multiple providers' do
+        let(:params) do
           {
-            'collectd' =>
+            providers:
             {
-              'connector' =>
+              'collectd' =>
               {
-                'path' => '/var/lib/collectd/rrd',
-                'type' => 'rrd'
-              }
-            },
-            'influxdb' =>
-            {
-              'connector' =>
+                'connector' =>
+                {
+                  'path' => '/var/lib/collectd/rrd',
+                  'type' => 'rrd'
+                }
+              },
+              'influxdb' =>
               {
-                'type' => 'influxdb',
-                'database' => 'webapps'
+                'connector' =>
+                {
+                  'type' => 'influxdb',
+                  'database' => 'webapps'
+                }
               }
             }
           }
+        end
+
+        it do
+          is_expected.to contain_file('facette-collectd.json').with(
+            ensure: 'file',
+            path: '/etc/facette/providers/collectd.json',
+            require: 'Package[facette]',
+            notify: 'Service[facette]',
+            owner: 'root',
+            group: 'root',
+            mode: '0644'
+          ).with_content(
+            %r{"connector":}
+          ).with_content(
+            %r{"path": "/var/lib/collectd/rrd"}
+          ).with_content(
+            %r{"type": "rrd"}
+          )
+        end
+        it do
+          is_expected.to contain_file('facette-influxdb.json').with(
+            ensure: 'file',
+            path: '/etc/facette/providers/influxdb.json',
+            require: 'Package[facette]',
+            notify: 'Service[facette]',
+            owner: 'root',
+            group: 'root',
+            mode: '0644'
+          ).with_content(
+            %r{"connector":}
+          ).with_content(
+            %r{"database": "webapps"}
+          ).with_content(
+            %r{"type": "influxdb"}
+          )
+        end
+      end
+    end
+
+    describe 'on unsupported' do
+      let(:facts) do
+        {
+          lsbdistcodename:    'trusty',
+          lsbdistrelease:     '14.04',
+          lsbmajdistrelease:  '14.04',
+          lsbdistdescription: 'Ubuntu 14.04.3 LTS',
+          lsbdistid:          'Banana',
+          osfamily:           'Debian',
+          operatingsystem:    'Ubuntu'
         }
       end
 
-      it do
-        is_expected.to contain_file('facette-collectd.json').with(
-          ensure: 'file',
-          path: '/etc/facette/providers/collectd.json',
-          require: 'Package[facette]',
-          notify: 'Service[facette]',
-          owner: 'root',
-          group: 'root',
-          mode: '0644'
-        ).with_content(
-          %r{"connector":}
-        ).with_content(
-          %r{"path": "/var/lib/collectd/rrd"}
-        ).with_content(
-          %r{"type": "rrd"}
-        )
-      end
-      it do
-        is_expected.to contain_file('facette-influxdb.json').with(
-          ensure: 'file',
-          path: '/etc/facette/providers/influxdb.json',
-          require: 'Package[facette]',
-          notify: 'Service[facette]',
-          owner: 'root',
-          group: 'root',
-          mode: '0644'
-        ).with_content(
-          %r{"connector":}
-        ).with_content(
-          %r{"database": "webapps"}
-        ).with_content(
-          %r{"type": "influxdb"}
-        )
-      end
-    end
-  end
-
-  describe 'on unsupported' do
-    let(:facts) do
-      {
-        lsbdistcodename:    'trusty',
-        lsbdistrelease:     '14.04',
-        lsbmajdistrelease:  '14.04',
-        lsbdistdescription: 'Ubuntu 14.04.3 LTS',
-        lsbdistid:          'Banana',
-        osfamily:           'Debian',
-        operatingsystem:    'Ubuntu'
-      }
-    end
-
-    it do
-      expect { subject.call }.to raise_error(Puppet::Error, %r{This module only supports Ubuntu})
+      it { is_expected.not_to compile }
     end
   end
 end
